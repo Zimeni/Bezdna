e43cbe2d8079505c37d8f87418ed317da8c36ed5
ZImeni <zimeni22@gmail.com>
2018-04-30 16:07:51 +0800

    Add libraries

---
+++ app/src/main/java/com/limore/zhibek/bezdna_reader/BooksCatalogActivity.java
@@ -0,0 +1,13 @@
+package com.limore.zhibek.bezdna_reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BooksCatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_catalog);
+    }
+}
